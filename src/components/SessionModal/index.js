cf848be9752c18f011ef5bcb8e462d59b3489921
Enmanuel Chipana <emachipana0102@gmail.com>
2023-09-28 09:23:13 -0500

    ADD: session modal component

---
+++ src/components/SessionModal/index.js
@@ -0,0 +1,223 @@
+import { Alert, Modal, Spinner } from "reactstrap";
+import { IoClose } from "react-icons/io5";
+import { 
+  Close,
+  Container,
+  Form,
+  Image,
+  Item,
+  Navigation,
+  Section,
+  SessionSection } from "./styles";
+import { useState } from "react";
+import { useAuth } from "../../context/auth";
+import { Formik } from "formik";
+import validate from "./validate";
+import Login from "./login";
+import Register from "./register";
+import Button from "../Button";
+
+const personInfoObject = {
+  dni: "",
+  name: "",
+  last_name: "",
+  error: {
+    dni: ""
+  },
+  blur: {
+    dni: false
+  }
+};
+
+function SessionModal({ setModal, modal }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [personInfo, setPersonInfo] = useState(personInfoObject);
+  const { setError, error, login, signup } = useAuth();
+
+  const toggle = (resetForm) => {
+    setModal({ ...modal, isOpen: !modal.isOpen });
+    setError(null);
+    setIsLoading(false);
+    setPersonInfo(personInfoObject);
+    resetForm();
+  }
+
+  const changeAction = (to, resetForm) => {
+    if(isLoading) return;
+
+    setModal(modal => ({...modal, action: to}));
+    setError(null);
+    resetForm();
+  }
+
+  const handleSubmit = async (values) => {
+    setIsLoading(true);
+    let data = values;
+
+    if(modal.action === "register") {
+      data = {
+        ...values,
+        dni: personInfo.dni,
+        name: personInfo.name,
+        last_name: personInfo.last_name
+      }
+    }
+
+    try {
+      if(modal.action === "login") {
+        const user = await login(data);
+        if(user) toggle();
+        setIsLoading(false);
+        return;
+      }
+
+      const newUser = await signup(data);
+      if(newUser) toggle();
+      setIsLoading(false);
+    }catch(e) {
+      setError(e.message);
+      setIsLoading(false);
+    }   
+  }
+
+  const initialValues = modal.action === "login"
+    ? {
+        email: "",
+        password: ""
+      }
+    : {
+        email: "",
+        dni: "",
+        name: "",
+        last_name: "",
+        phone: "",
+        address: "",
+        password: "",
+        password_confirmation: ""
+      }
+    
+  return (
+    <Modal
+      isOpen={modal.isOpen}
+      toggle={toggle}
+      centered
+      size="lg"
+    >
+      <Container>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validate={(values) => validate(values, modal.action)}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            isValid,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            resetForm
+          }) => (
+            <>
+              <Close>
+                <IoClose
+                  onClick={() => toggle(resetForm)}
+                  size={25}
+                />
+              </Close>
+              <Section
+                isImage
+              >
+                <Image 
+                  alt="box"
+                  src="assets/photo/box.jpeg"
+                />
+              </Section>
+              <Section>
+                <SessionSection>
+                  <Navigation>
+                    <Item
+                      onClick={() => changeAction("register", resetForm)}
+                      active={modal.action === "register"}
+                    >
+                      Registro
+                    </Item>
+                    <Item
+                      onClick={() => changeAction("login", resetForm)}
+                      active={modal.action === "login"}
+                    >
+                      Acceder
+                    </Item>
+                  </Navigation>
+                  <Form
+                    action={modal.action}
+                    onSubmit={handleSubmit}
+                  >
+                    {
+                      modal.action === "login"
+                      ? <Login 
+                          errors={errors}
+                          handleBlur={handleBlur}
+                          handleChange={handleChange}
+                          touched={touched}
+                          values={values}
+                        />
+                      : <Register 
+                          errors={errors}
+                          handleBlur={handleBlur}
+                          handleChange={handleChange}
+                          touched={touched}
+                          values={values}
+                          personInfo={personInfo}
+                          setPersonInfo={setPersonInfo}
+                        />
+                    }
+                    {
+                      error
+                      ? <Alert
+                          color="danger"
+                          style={{width: "100%"}}
+                        >
+                          { error.replaceAll('"', "") }
+                        </Alert>
+                      : null
+                    }
+                    <Button
+                      filled
+                      color="secondary"
+                      size="full"
+                      disabled={
+                        !isValid
+                        || isLoading
+                        || (modal.action === "register" && !personInfo.dni) 
+                        || (modal.action === "register" && personInfo.dni.length < 8)
+                      }
+                      type="submit"
+                    >
+                      {
+                        isLoading
+                        ? <>
+                            <Spinner size="sm" />
+                            {" "}
+                            {
+                              modal.action === "login"
+                              ? "Iniciando sesión..."
+                              : "Registrando..."
+                            }
+                          </>
+                        : modal.action === "login" ? "Iniciar sesión" : "Registrarme"
+                      }
+                    </Button>
+                  </Form>
+                </SessionSection>
+              </Section>
+            </>
+          )}
+        </Formik>
+      </Container>
+    </Modal>
+  );
+}
+
+export default SessionModal;
---
+++ src/components/SessionModal/styles.js
@@ -0,0 +1,102 @@
+import { COLORS } from "../../styles/colors";
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  background-color: ${COLORS.orange};
+  min-height: 75vh;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+export const Close = styled.div`
+  position: absolute;
+  top: 5px;
+  right: 10px;
+  color: white;
+  cursor: pointer;
+`;
+
+export const Section = styled.section`
+  min-height: 75vh;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: 990px) {
+    width: ${({ isImage }) => isImage ? "40%" : "60%"};
+  }
+`;
+
+export const Image = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+`;
+
+export const SessionSection = styled.section`
+  width: 100%;
+  height: 100%;
+  padding: 1rem 0;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  @media screen and (max-width: 500px) {
+    padding: 0.5rem;
+  }
+`;
+
+export const Navigation = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5rem;
+  padding: 1.5rem 0;
+
+  @media screen and (max-width: 450px) {
+    gap: 0.5rem;
+  }
+`;
+
+export const Item = styled.button`
+  border: none;
+  padding: 0.5rem 2rem;
+  font-weight: 500;
+  border: 1px solid white;
+  background-color: ${({ active }) => active ? "white" : "transparent"};
+  color: ${({ active }) => active ? COLORS.black : "white"};
+  cursor: pointer;
+  transition: .3s ease-in;
+  font-size: 1rem;
+
+  &:hover {
+    background-color: white;
+    color: ${COLORS.black};
+  }
+
+  @media screen and (max-width: 500px) {
+    padding: 0.5rem 0.8rem;
+    font-size: 15px;
+  }
+
+  @media screen and (max-width: 350px) {
+    font-size: 14px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  padding: ${({ action }) => action === "login" ? "3rem" : "1rem"} 2.5rem;
+  gap: 1rem;
+
+  @media screen and (max-width: 800px) {
+    padding: 1rem;
+  }
+`;
