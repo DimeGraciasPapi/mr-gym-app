0ad92506e55a232ae1fe858cc12a2e1cf11a01e3
Enmanuel Chipana <emachipana0102@gmail.com>
2023-10-04 10:28:02 -0500

    FIX: conditional and render home page

--- src/App.js
+++ src/App.js
@@ -18,23 +18,23 @@ function App() {
           setModal={setModal}
         />
         {
-          user?.user_type === "admin"
-          ? "Admin section"
-          : <Routes>
-              <Route index path="/" element={<Home />} />
-              <Route path="/planes" element={<h1>Planes page</h1>} />
-              <Route path="/ubicanos" element={<h1>Ubicanos page</h1>} />
-              <Route path="*" element={<h1>Pagina no encontrada</h1>} />
-              {
-                user
-                ? <>
+          <Routes>
+            {
+              user && user.user_type === "client"
+              ? ""
+              : <Route index path="/" element={<Home setModal={setModal} />} />
+            }
+            <Route path="/planes" element={<h1>Planes page</h1>} />
+            <Route path="/ubicanos" element={<h1>Ubicanos page</h1>} />
+            <Route path="*" element={<h1>Pagina no encontrada</h1>} />
+            {
+              user
+              &&  <>
                     <Route path="/perfil" element={<h1>Profile page</h1>}/>
                     <Route path="/client-logged" element={<h1>pagina solo para cliente loggeado</h1>} />
                   </> 
-                
-                : null
-              }
-            </Routes>
+            }
+          </Routes>
         }
         <SessionModal 
           modal={modal}
