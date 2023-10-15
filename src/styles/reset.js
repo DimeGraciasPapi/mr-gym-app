56c78a5e5b83a7ed0918b884ff8523210008fb08
Enmanuel Chipana <emachipana0102@gmail.com>
2023-10-14 19:08:27 -0500

    ADD: font montserrat to index styles

--- src/styles/reset.js
+++ src/styles/reset.js
@@ -1,9 +1,10 @@
 import { css } from "@emotion/react";
 import { COLORS } from "./colors";
+import { FONT } from "./font";
 
 export const RESET = css`
-  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
-
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  
   * {
     padding: 0;
     margin: 0;
@@ -21,6 +22,7 @@ export const RESET = css`
   }
 
   body {
+    font-family: ${FONT.primary};
     font-size: 1rem;
     color: ${COLORS.black};
     background-color: ${COLORS.white};
