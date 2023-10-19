58ab091171585984f692bef45ed8840fc2dae84c
Enmanuel Chipana <emachipana0102@gmail.com>
2023-10-19 10:43:29 -0500

    ADD: icon marker for map

---
+++ src/pages/Client/Ubication/iconMarker.js
@@ -0,0 +1,11 @@
+import L from "leaflet";
+
+export default L.icon({
+  iconUrl: "assets/logo.png",
+  iconRetinaUrl: "assets/logo.png",
+  iconAnchor: null,
+  shadowUrl: null,
+  shadowAnchor: null,
+  iconSize: [50, 50],
+  className: "leaflet-venue-icon"
+});
