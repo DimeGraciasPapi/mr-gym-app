ca5b5bfe84c8c7fb2af977eaedbec3d4b8a39477
Enmanuel Chipana <emachipana0102@gmail.com>
2023-11-07 10:26:27 -0500

    ADD: mr gym go page

--- src/pages/Client/MrGymGo/index.js
+++ src/pages/Client/MrGymGo/index.js
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../../../components/Button";
 import { useData } from "../../../context/data";
 import { Container } from "../styles";
-import { FlexColumn, Icon, Section, Text, Title } from "./styles";
+import { Card, FlexColumn, GymSection, Icon, Image, Section, Text, Title } from "./styles";
+import { GiBiceps } from "react-icons/gi";
 
 function MrGymGo() {
   const { setModal } = useData();
+  const navigate = useNavigate();
 
   return (
     <>
@@ -34,6 +37,105 @@ function MrGymGo() {
           </FlexColumn>
         </Section>
       </Container>
+      <GymSection src_img="gym_1.jpeg">
+        <Card isImage>
+          <Image 
+            alt="mr_gym_go"
+            src="assets/photo/gym_1.jpeg"
+          />
+        </Card>
+        <Card>
+          <Title
+            size={1.6}
+            color="black"
+          >
+            Cardio y Ritmo
+          </Title>
+          <Text size={15}>
+            ¿Estás buscando una manera de mejorar tu salud
+            cardiovascular? ¡Tenemos la solución perfecta
+            para ti en nuestro gimnasio! Ofrecemos una amplia
+            variedad de máquinas de cardio, desde cintas de
+            correr y bicicletas estáticas, todas diseñadas
+            para ayudarte a aumentar tu resistencia y mejorar
+            la salud de tu corazón.
+          </Text>
+          <Button
+            onClick={() => navigate("cardio-ritmo")}
+            filled
+            Icon={GiBiceps}
+          >
+            Vamos a por ello
+          </Button>
+        </Card>
+      </GymSection>
+      <GymSection src_img="gym_2.jpeg">
+        <Card>
+          <Title
+            size={1.6}
+            color="black"
+          >
+            Fuerza y Resistencia
+          </Title>
+          <Text size={15}>
+            ¿Estás buscando una manera de mejorar tu salud
+            cardiovascular? ¡Tenemos la solución perfecta
+            para ti en nuestro gimnasio! Ofrecemos una amplia
+            variedad de máquinas de cardio, desde cintas de
+            correr y bicicletas estáticas, todas diseñadas
+            para ayudarte a aumentar tu resistencia y mejorar
+            la salud de tu corazón.
+          </Text>
+          <Button
+          onClick={() => navigate("fuerza-resistencia")}
+            filled
+            Icon={GiBiceps}
+          >
+            Vamos a por ello
+          </Button>
+        </Card>
+        <Card isImage>
+          <Image 
+            alt="mr_gym_go"
+            src="assets/photo/gym_2.jpeg"
+          />
+        </Card>
+      </GymSection>
+      <GymSection 
+        src_img="gym_3.jpeg"
+        positionY={50}
+      >
+        <Card isImage>
+          <Image 
+            alt="mr_gym_go"
+            src="assets/photo/gym_3.jpeg"
+          />
+        </Card>
+        <Card>
+          <Title
+            size={1.6}
+            color="black"
+          >
+            Cuerpo y mente
+          </Title>
+          <Text size={15}>
+            ¿Quieres fortalecer tus músculos y mejorar tu
+            fuerza física? ¡Tenemos lo que necesitas en
+            nuestro gimnasio! Contamos con una amplia
+            variedad de equipos de entrenamiento de fuerza,
+            desde pesas libres y máquinas de musculación
+            hasta entrenamientos de alta intensidad y clases
+            de levantamiento de pesas.
+          </Text>
+          <Button
+            onClick={() => navigate("cuerpo-mente")}
+            filled
+            Icon={GiBiceps}
+          >
+            Vamos a por ello
+          </Button>
+        </Card>
+      </GymSection>
     </>
   );
 }
--- src/pages/Client/MrGymGo/styles.js
+++ src/pages/Client/MrGymGo/styles.js
@@ -27,15 +27,16 @@ export const FlexColumn = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-size: 2.5rem;
+  font-size: ${({ size }) => size || 2.5}rem;
   font-weight: 800;
   text-align: center;
-  color: white;
+  color: ${({ color }) => color || "white"};
+  margin: 0;
 `;
 
 export const Text = styled.p`
   color: white;
-  font-size: 19px;
+  font-size: ${({ size }) => size || 19}px;
   text-align: center;
   font-weight: 500;
 `;
@@ -46,4 +47,52 @@ export const Icon = styled(BiDumbbell)`
   padding: 1rem;
   border-radius: 50%;
   background-color: black;
+`;
+
+export const GymSection = styled.div`
+  width: 100%;
+  min-height: 420px;
+  padding: 2rem;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("assets/photo/${({ src_img }) => src_img}");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center ${({positionY}) => positionY || 20}%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 2rem;
+
+  @media screen and (max-width: 520px) {
+    padding: 2rem 1rem;
+  }
+`;
+
+export const Card = styled.div`
+  min-height: 300px;
+  height: ${({ isImage }) => isImage ? "300px" : ""};
+  width: 480px;
+  padding: ${({isImage}) => isImage ? 0 : "1rem"};
+  gap: 1rem;
+  border: 2px solid white;
+  border-radius: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition: .2s ease-in;
+  overflow: hidden;
+  background: linear-gradient(180deg, #E1631D, #010101);;
+
+  &:hover {
+    transform: translate(4px, -4px);
+    box-shadow: 4px -4px 8px 2px rgba(255, 255, 255, .2);
+  }
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
