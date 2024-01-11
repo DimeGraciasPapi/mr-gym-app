bb0f67f637b1d886047673be4ccc4d20d9ac7a88
Enmanuel Chipana <emachipana0102@gmail.com>
2024-01-10 22:20:39 -0500

    ADD: navbar for admin

---
+++ src/components/Navbar/Admin/styles.js
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+import { COLORS } from "../../../styles/colors";
+
+export const Container = styled.div`
+  grid-area: 1 / 1 / 2 / 3;
+  height: 80px;
+  width: 100%;
+  background-color: ${COLORS.black};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  top: 0;
+  position: sticky;
+  padding: 1rem 2rem;
+
+  .menu {
+    cursor: pointer;
+    font-size: 25px;
+    display: none;
+    color: white;
+
+    @media screen and (max-width: 800px) {
+      display: block;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    padding: 1rem;
+  }
+`;
+
+export const Logo = styled.img`
+  height: 62px;
+  width: 62px;
+  object-fit: cover;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+export const FlexRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`;
