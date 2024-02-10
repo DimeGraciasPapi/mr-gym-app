4bf8e219c1baeab0e0d3abe785fb6cbc8fb1176c
DimeGraciasPapi <enmavilchezsanchez@gmail.com>
2024-02-10 13:23:22 -0500

    finish

--- src/components/chatbot/StartSlow/StartSlow.js
+++ src/components/chatbot/StartSlow/StartSlow.js
@@ -8,7 +8,7 @@ function StartSlow(props){
         props.actions.horariosInfo()
     }
     const normasGym = () =>{
-        window.open('https://drive.google.com/file/d/1Or-cW6Ysjk9DM2yJuBrsZiFeHjMQQpJ7/view?usp=sharing');
+        window.open('https://drive.google.com/drive/folders/1qiagmCxkG2ykbfuCh8YRPvW138Yq2iPz?usp=sharing');
     }
     const ubication = () =>{
         window.open('https://maps.app.goo.gl/imnBc8vD3kvJ5xVt8');
@@ -22,7 +22,7 @@ function StartSlow(props){
             <button className="start-btn" onClick={()=> horariosInfo()}>HORARIOS</button>
             <button className="start-btn" onClick={() => ubication()}>UBICACIÓN</button>
             <button className="start-btn" onClick={() => redesSociales()}>REDES SOCIALES</button>
-            <button className="start-btn" onClick={() => normasGym()}>POLÍTICAS DEL GYM</button>
+            <button className="start-btn" onClick={() => normasGym()}>DESCARGAR APK</button>
         </div>
     )
 }
